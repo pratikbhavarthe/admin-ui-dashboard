89f7853582227f91f2fcee1f673c65b83dd4e199
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2023-12-03 18:20:08 +0530

    Added a new services folder in src and nested a common subfolder containing the initial httpService.js file  🔌

---
+++ src/services/common/httpService.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { toast } from "react-toastify";
+import logger from "./logService";
+
+axios.defaults.baseURL = process.env.ADMIN_UI_API_URL || 'https://geektrust.s3-ap-southeast-1.amazonaws.com';
+
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    logger.log(error);
+    toast.error("An unexpected error occurrred.");
+  }
+
+  return Promise.reject(error);
+});
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+};
