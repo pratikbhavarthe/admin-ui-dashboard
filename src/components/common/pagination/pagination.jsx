335f6a987497a81235963a45177ee1ea0fc55d22
Pratik Bhavarthe <pratikbhavarthee1998@gmail.com>
2023-12-03 19:15:09 +0530

    Optimize Pagination component

--- src/components/common/pagination/pagination.jsx
+++ src/components/common/pagination/pagination.jsx
@@ -1,81 +1,64 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import { range } from '../../../utils'
+import { range } from '../../../utils';
 
-const Pagination = (props) => {
-  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
   const pagesCount = Math.ceil(itemsCount / pageSize);
-  if (pagesCount === 1) return null;
-  const pages = range(1, pagesCount);
+
+  if (pagesCount === 1) {
+    return null;
+  }
+
+  let pages = range(1, pagesCount);
+  const disabledClass = "page-item disabled";
 
   return (
     <div className="float-end">
       <nav aria-label="Page navigation">
         <ul className="pagination">
-          <li
-            className={currentPage === 1 ? "page-item disabled" : "page-item"}
-          >
-            <a
-              className="page-link"
-              aria-label="first"
-              onClick={() => onPageChange(1)}
-            >
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li
-            className={currentPage === 1 ? "page-item disabled" : "page-item"}
-          >
-            <a
-              className="page-link"
-              aria-label="Previous"
-              onClick={() => onPageChange(currentPage - 1)}
-            >
-              <span aria-hidden="true">&lt;</span>
-            </a>
-          </li>
+          <PaginationButton
+            className={currentPage === 1 ? disabledClass : "page-item"}
+            onClick={() => onPageChange(1)}
+            label="&laquo;"
+          />
+          <PaginationButton
+            className={currentPage === 1 ? disabledClass : "page-item"}
+            onClick={() => onPageChange(currentPage - 1)}
+            label="&lt;"
+          />
           {pages.map((page) => (
-            <li
+            <PaginationButton
               key={page}
-              className={
-                page === currentPage ? "page-item active" : "page-item"
-              }
-            >
-              <a onClick={() => onPageChange(page)} className="page-link">
-                {page}
-              </a>
-            </li>
+              className={page === currentPage ? "page-item active" : "page-item"}
+              onClick={() => onPageChange(page)}
+              label={page.toString()}
+            />
           ))}
-          <li
-            className={
-              currentPage === pages.length ? "page-item disabled" : "page-item"
-            }
-          >
-            <a
-              className="page-link"
-              aria-label="Next"
-              onClick={() => onPageChange(currentPage + 1)}
-            >
-              <span aria-hidden="true">&gt;</span>
-            </a>
-          </li>
-          <li
-            className={
-              currentPage === pages.length ? "page-item disabled" : "page-item"
-            }
-          >
-            <a
-              className="page-link"
-              aria-label="last"
-              onClick={() => onPageChange(pages.length)}
-            >
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
+          <PaginationButton
+            className={currentPage === pages.length ? disabledClass : "page-item"}
+            onClick={() => onPageChange(currentPage + 1)}
+            label="&gt;"
+          />
+          <PaginationButton
+            className={currentPage === pages.length ? disabledClass : "page-item"}
+            onClick={() => onPageChange(pages.length)}
+            label="&raquo;"
+          />
         </ul>
       </nav>
     </div>
   );
 };
+
+const PaginationButton = ({ className, onClick, label }) => (
+  <li className={className}>
+    <button
+      className="page-link"
+      onClick={onClick}
+      role="button"
+    >
+      <span aria-hidden="true">{label}</span>
+    </button>
+  </li>
+);
 
 export default Pagination;
